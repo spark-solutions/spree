3f416662c084d8d2ff7756e1f34cb6e04d9a93e6
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2017-05-14 13:37:45 +0200

    [rails 5.0.3] fix order inventory model specs

--- core/spec/models/spree/order_inventory_spec.rb
+++ core/spec/models/spree/order_inventory_spec.rb
@@ -212,7 +212,8 @@ describe Spree::OrderInventory, type: :model do
       end
 
       it 'should destroy self if not inventory units remain' do
-        allow(shipment.inventory_units).to receive_messages(count: 0)
+        allow(shipment).to receive(:inventory_units).and_return(shipment.inventory_units)
+        allow(shipment.inventory_units).to receive_messages(sum: 0)
         expect(shipment).to receive(:destroy)
 
         expect(subject.send(:remove_from_shipment, shipment, 1)).to eq(1)
