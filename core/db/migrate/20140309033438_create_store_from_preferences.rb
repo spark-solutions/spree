ebeee5074ec03ef85409b2816029ea45eb46d853
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-03-06 18:39:57 +0100

    use class with spree_stores to fix problems with cached Spree::Store

--- core/db/migrate/20140309033438_create_store_from_preferences.rb
+++ core/db/migrate/20140309033438_create_store_from_preferences.rb
@@ -1,21 +1,24 @@
 class CreateStoreFromPreferences < ActiveRecord::Migration
-  def change
-    # workaround for spree_i18n and Store translations
-    Spree::Store.class_eval do
-      def self.translated?(name)
-        false
-      end
+
+  # workaround for spree_i18n and Store translations
+  # Spree::Store.reset_column_information doesnt work as expected
+  class Store < Spree::Base
+    self.table_name = 'spree_stores'
+    def self.translated?(name)
+      false
     end
-    
+  end
+
+  def change
     preference_store = Spree::Preferences::Store.instance
-    if store = Spree::Store.where(default: true).first
+    if store = Store.where(default: true).first
       store.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
       store.meta_keywords    = preference_store.get('spree/app_configuration/default_meta_keywords') {}
       store.seo_title        = preference_store.get('spree/app_configuration/default_seo_title') {}
       store.save!
     else
       # we set defaults for the things we now require
-      Spree::Store.new do |s|
+      Store.new do |s|
         s.name              = preference_store.get 'spree/app_configuration/site_name' do
           'Spree Demo Site'
         end
