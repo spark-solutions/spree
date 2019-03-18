a10493bfafe8b2cc00f2dd7d6b8c85584e5fe59c
jkojro <janusz.kojro@sparksoluitons.co>
2019-03-18 16:45:54 +0100

    Add Estimate Shipping Rates API v2 endpoint

--- core/app/services/spree/cart/estimate_shipping_rates.rb
+++ core/app/services/spree/cart/estimate_shipping_rates.rb
@@ -5,10 +5,10 @@ module Spree
 
       def call(order:, country_iso: nil)
         country_id = if country_iso.present?
-          ::Spree::Country.by_iso(country_iso).id
-        else
-          ::Spree::Country.default.id
-        end
+                       ::Spree::Country.by_iso(country_iso).id
+                     else
+                       ::Spree::Country.default.id
+                     end
 
         dummy_order = order.dup
         dummy_order.line_items = order.line_items
@@ -16,10 +16,10 @@ module Spree
         packages = ::Spree::Stock::Coordinator.new(dummy_order).packages
         estimator = ::Spree::Stock::Estimator.new(dummy_order)
         shipping_rates = if order.line_items.any? && packages.any?
-                            estimator.shipping_rates(packages.first)
-                          else
-                            []
-                          end
+                           estimator.shipping_rates(packages.first)
+                         else
+                           []
+                         end
 
         success(shipping_rates)
       end
