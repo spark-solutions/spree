f63e9901cd044018486a4f29a44a8c3d796dadc6
Damian Legawiec <damian@sparksolutions.co>
2019-05-08 16:13:35 +0200

    Don't include bootsnap and webpacker in  sandbox

--- lib/sandbox.sh
+++ lib/sandbox.sh
@@ -27,7 +27,9 @@ bundle exec rails new sandbox --database="$RAILSDB" \
   --skip-rc \
   --skip-spring \
   --skip-test \
-  --skip-coffee
+  --skip-coffee \
+  --skip-javascript \
+  --skip-bootsnap
 
 if [ ! -d "sandbox" ]; then
   echo 'sandbox rails application failed'
@@ -67,4 +69,3 @@ bundle exec rails db:create
 bundle exec rails g spree:install --auto-accept --user_class=Spree::User --enforce_available_locales=true --copy_views=false
 bundle exec rails g spree:auth:install
 bundle exec rails g spree_gateway:install
-bundle exec rails webpacker:install
