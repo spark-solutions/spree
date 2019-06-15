2bd9cb5762211901d03f09cc00b1743bb835a936
Damian Legawiec <damian@sparksolutions.co>
2019-06-15 21:45:25 +0200

    eslint fix

--- frontend/app/assets/javascripts/spree/frontend/multi_currency.js
+++ frontend/app/assets/javascripts/spree/frontend/multi_currency.js
@@ -1,11 +1,11 @@
 $(() =>
-  $('#currency').on('change', function() {
+  $('#currency').on('change', function () {
     return $.ajax({
       type: 'GET',
       url: $(this).data('href'),
       data: {
         currency: $(this).val()
       }
-    }).done(() => window.location.reload());
+    }).done(() => window.location.reload())
   })
-);
+)
