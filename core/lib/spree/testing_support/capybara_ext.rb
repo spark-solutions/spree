db2afa057c5f3dd3bed1f356cb66aaa2342c0cef
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-08-23 14:36:45 +0200

    move method to proper place

--- core/lib/spree/testing_support/capybara_ext.rb
+++ core/lib/spree/testing_support/capybara_ext.rb
@@ -85,6 +85,16 @@ module CapybaraExt
     end
   end
 
+  def wait_for_condition(delay = Capybara.default_max_wait_time)
+    counter = 0
+    delay_threshold = delay * 10
+    until yield
+      counter += 1
+      sleep(0.1)
+      raise "Could not achieve condition within #{delay} seconds." if counter >= delay_threshold
+    end
+  end
+
   def disable_html5_validation
     page.execute_script('for(var f=document.forms,i=f.length;i--;)f[i].setAttribute("novalidate",i)')
   end
@@ -96,16 +106,6 @@ def wait_for(options = {})
   Selenium::WebDriver::Wait.new(timeout: default_options[:seconds]).until { yield }
 rescue Selenium::WebDriver::Error::TimeOutError
   default_options[:error].nil? ? false : raise(default_options[:error])
-end
-
-def wait_for_condition(delay = Capybara.default_max_wait_time)
-  counter = 0
-  delay_threshold = delay * 10
-  until yield
-    counter += 1
-    sleep(0.1)
-    raise "Could not achieve condition within #{delay} seconds." if counter >= delay_threshold
-  end
 end
 
 Capybara.configure do |config|
