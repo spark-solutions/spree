fcdabdf51a2b3fbfb90e3fb02f434627d96484bb
Damian Legawiec <damian@sparksolutions.co>
2020-04-23 22:32:25 +0200

    fix

--- api/app/controllers/spree/api/errors_controller.rb
+++ api/app/controllers/spree/api/errors_controller.rb
@@ -1,8 +1,6 @@
 module Spree
   module Api
     class ErrorsController < ActionController::API
-      protect_from_forgery
-
       def render_404
         render 'spree/api/errors/not_found', status: 404
       end
